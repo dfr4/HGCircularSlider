448477176b6bc9ca28f3bc18955e605f4dfe9f82
Daniel Rascon <user@google.com>
2019-08-15 15:41:09 -0700

    New 4

--- HGCircularSlider/Classes/CircularSliderHelper.swift
+++ HGCircularSlider/Classes/CircularSliderHelper.swift
@@ -185,7 +185,7 @@ internal class CircularSliderHelper {
         default:
             let sourceRange = (source.max - source.min) / CGFloat(source.rounds)
             let destinationRange = (destination.max - destination.min) / CGFloat(destination.rounds)
-            let scaledValue = source.min + (value - source.min).truncatingRemainder(dividingBy: sourceRange)
+            let scaledValue = scaleValue(deltaAngle, fromInterval: angleIntreval, toInterval: interval) - interval.min
             let newValue = (((scaledValue - source.min) * destinationRange) / sourceRange)
             
             return  newValue
